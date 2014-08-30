68b645f051ee32a3ff36b21a2aeb66358bd5f760
Eduardo Borges <eduardomrb@gmail.com>
2014-08-30 15:43:16 -0400

    create mongoid.yml config file and boot Mongoid on app.rb

--- app.rb
+++ app.rb
@@ -1,9 +1,12 @@
 require "sinatra"
 require "sinatra/cross_origin"
+require "mongoid"
 
 configure do
   enable :cross_origin
 end
+
+Mongoid.load!(File.join(File.dirname(__FILE__), "config/mongoid.yml"))
 
 get "/" do
   "haraw"
