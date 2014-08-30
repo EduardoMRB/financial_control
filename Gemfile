d64e1d96cc975094673ea543ae58341ee51e1eb8
Eduardo Borges <eduardomrb@gmail.com>
2014-08-30 14:19:58 -0400

    add mongoid as dependency and create Outgoing document

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source "https://rubygems.org"
 
 gem "sinatra"
 gem "sinatra-cross_origin", "~> 0.3.1"
+gem "mongoid"
 
 group :development, :test do
   gem "rspec"
---
+++ lib/financial_control/document/outgoing.rb
@@ -0,0 +1,9 @@
+module FinancialControl
+  class Outgoing
+    include Mongoid::Document
+
+    field :date,           type: Date
+    field :description,    type: String
+    field :payment_method, type: String
+  end
+end
