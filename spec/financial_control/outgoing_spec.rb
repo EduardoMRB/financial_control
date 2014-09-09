6ccc0270e71115c3b609d2eea69f6fd3d98c47d0
Eduardo Borges <eduardomrb@gmail.com>
2014-09-08 23:37:41 -0400

    Refactor Outgoing Specs.

--- spec/financial_control/outgoing_spec.rb
+++ spec/financial_control/outgoing_spec.rb
@@ -1,25 +1,23 @@
 require "financial_control/document/outgoing"
 
 describe FinancialControl::Outgoing do
-  before :each do
-    @date = Date.new
-    @outgoing = FinancialControl::Outgoing.new(
-      date: @date, description: "haircut", payment_method: "debit")
-  end
+  let(:outgoing) { FinancialControl::Outgoing.new(args) }
+  let(:args) { { date: Date.new, description: "haircut", payment_method: "Debit" } }
 
   it "should have Mongoid::Document included" do
     expect(FinancialControl::Outgoing.include?(Mongoid::Document)).to be true
   end
 
   it "should have date, description and payment_method attr" do
-
-    expect(@outgoing.date).to eq @date
-    expect(@outgoing.description).to eq "haircut"
-    expect(@outgoing.payment_method).to eq "debit"
+    Timecop.freeze do
+      expect(outgoing.date).to eq Date.new
+      expect(outgoing.description).to eq "haircut"
+      expect(outgoing.payment_method).to eq "Debit"
+    end
   end
 
   it "should respond to save message" do
-    expect(@outgoing).to respond_to(:save)
+    expect(outgoing).to respond_to(:save)
   end
 
   it "should have default date value" do
